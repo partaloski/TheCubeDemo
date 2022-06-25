45947280233dd35c46326989e7c46fb0160a8e7e
Petar Partaloski <ppartaloski@yahoo.com>
2022-06-25 19:57:09 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+	include_once("index.html");
+	include_once("img/AlphaTauri.png");
+	include_once("img/AstonMartin.png");
+	include_once("img/Alpine.png");
+	include_once("img/FERRARI.png");
+	include_once("img/McLaren.png");
+	include_once("img/Mercedes.png");
+	include_once("img/RedBull.png");
+ ?>
