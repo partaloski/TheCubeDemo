c35877ad59fef30090ea48c1fe9be55ace9f422d
Petar Partaloski <ppartaloski@yahoo.com>
2022-06-25 20:22:28 +0200

    Added adding of cubes.

---
+++ cube_demo/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#linkDiv").hide()
+    $("#videoDiv").hide()
+    $("#imageDiv").hide()
+    $("#twitterDiv").hide()
+
+    $("#choice").change(function(){
+        let val = $("#choice option:selected").val()
+
+        $("#linkDiv").hide()
+        $("#videoDiv").hide()
+        $("#imageDiv").hide()
+        $("#twitterDiv").hide()
+
+        if(val==='1')
+            $("#linkDiv").show()
+        if(val==='2')
+            $("#twitterDiv").show()
+        if(val==='3')
+            $("#videoDiv").show()
+        if(val==='4')
+            $("#imageDiv").show()
+    })
+})
