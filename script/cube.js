875a52b18e1b1840a8775ad1c7589b564b86b59b
Petar Partaloski <ppartaloski@yahoo.com>
2022-06-30 15:48:47 +0200

    Added share link to cube

--- script/cube.js
+++ script/cube.js
@@ -1,14 +1,24 @@
 $(document).ready(function (){
-    $("#toggleCubeVisibility").click(function () {
-        if($("#visibility").text() === 'Visible'){
-            $("#visibility").text('Hidden')
-        }
-        else{
-            $("#visibility").text('Visible')
-        }
-        $("#visibility").toggleClass("text-warning")
-        $("#visibility").toggleClass("text-info")
-        $("#toggleCubeVisibility").toggleClass("btn-warning")
-        $("#toggleCubeVisibility").toggleClass("btn-info")
+    $("#invisibleIcon").hide()
+    let button = $("#toggleCubeVisibility")
+    let label = $("#visibility")
+    let buttonCopy = $("#buttonCopy")
+    let buttonCopyLabel = $("#buttonCopyLabel")
+
+    $(button).click(function () {
+        $("#visibleIcon").fadeToggle()
+        $("#invisibleIcon").fadeToggle()
+        label.toggleClass("text-warning")
+        label.toggleClass("text-info")
+        button.toggleClass("btn-warning")
+        button.toggleClass("btn-info")
     })
+
+    $(buttonCopy).click(function (){
+        navigator.clipboard.writeText("https://the-cube-demo.herokuapp.com/cubes/cube_name")
+        $(buttonCopyLabel).text("Link copied!")
+        $(buttonCopy).addClass("btn-success")
+        $(buttonCopy).removeClass("btn-info")
+    })
+
 })
