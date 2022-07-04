91fd6a45f1db3763d145f26a2b30aa0e4d375ce9
Petar Partaloski <ppartaloski@yahoo.com>
2022-07-04 12:48:29 +0200

    Added forgot password feature and checking of required fields

--- script/admin.js
+++ script/admin.js
@@ -3,5 +3,24 @@ $(document).ready(function (){
         if($("#username").val() !== '' && $("#password").val() !== ''){
             document.getElementById("redirect").click()
         }
+        else{
+            $("#errorsLogin").fadeIn()
+        }
+    })
+
+    $("#errorsLogin").hide()
+    $("#forgotPasswordConfirmationDiv").slideToggle()
+    $("#errorsForgotPassword").slideToggle()
+
+    $("#forgotPasswordContinueBtn").click(function (){
+        if($("#email").val() !== ''){
+            $("#errorsForgotPassword").slideUp()
+            $("#inputs").addClass("opacity-75")
+            $("#forgotPasswordConfirmationDiv").slideDown()
+            $("#forgotPasswordContinueBtn").prop('disabled', true);
+        }
+        else{
+            $("#errorsForgotPassword").fadeIn()
+        }
     })
 })
--- script/creator.js
+++ script/creator.js
@@ -3,5 +3,25 @@ $(document).ready(function (){
         if($("#username").val() !== '' && $("#password").val() !== ''){
             document.getElementById("redirect").click()
         }
+        else{
+            $("#errorsLogin").fadeIn()
+        }
+    })
+
+    $("#errorsLogin").hide()
+    $("#forgotPasswordConfirmationDiv").hide()
+    $("#errorsForgotPassword").hide()
+
+
+    $("#forgotPasswordContinueBtn").click(function (){
+        if($("#email").val() !== ''){
+            $("#errorsForgotPassword").slideUp()
+            $("#inputs").addClass("opacity-75")
+            $("#forgotPasswordConfirmationDiv").slideDown()
+            $("#forgotPasswordContinueBtn").prop('disabled', true);
+        }
+        else{
+            $("#errorsForgotPassword").fadeIn()
+        }
     })
 })
