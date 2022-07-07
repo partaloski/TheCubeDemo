d7f7067ac0396124e9e1fbcf8933825e4b06ae66
Petar Partaloski <ppartaloski@yahoo.com>
2022-07-07 10:12:09 +0200

    Added preview link

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php 
 	include_once("index.html");
-	include_once("img/AlphaTauri.png");
-	include_once("img/AstonMartin.png");
-	include_once("img/Alpine.png");
-	include_once("img/FERRARI.png");
-	include_once("img/McLaren.png");
-	include_once("img/Mercedes.png");
-	include_once("img/RedBull.png");
  ?>
