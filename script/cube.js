573766e34909ec006da6d4135201b8b790f615a5
Petar Partaloski <ppartaloski@yahoo.com>
2022-07-11 16:47:28 +0200

    Added new way of Images being selected and ordered

--- script/cube.js
+++ script/cube.js
@@ -21,11 +21,80 @@ $(document).ready(function (){
         $(buttonCopy).removeClass("btn-info")
     })
 
-    let imageSelect = $("#images")
+    let imagesLeft = $("#imagesLeft")
+    let imagesSelected = $("#imagesSelected")
 
     for(let i=0; i<10; i++){
         let value = "Image " + (i+1)
-        $(imageSelect).append("<option>"+value+"</option>")
+        $(imagesLeft).append("<option>"+value+"</option>")
     }
 
+    $("#btnGoLeft").click(function (){
+        let selected = $("#imagesLeft option:selected")
+        imagesSelected.append(selected)
+        imagesLeft.remove(selected)
+        selected.prop('selected','')
+    })
+
+    $("#btnGoRight").click(function (){
+        let selected = $("#imagesSelected option:selected")
+        imagesLeft.append(selected)
+        imagesSelected.remove(selected)
+        selected.prop('selected','')
+    })
+
+    $("#btnMoveUp").click(function (){
+        let options = $("#imagesSelected option")
+        let selected = $("#imagesSelected option:selected")
+        for(let i=1; i<options.length; i++){
+            for(let j=selected.length-1; j>=0; j--){
+                if(options[i] === selected[j]){
+                    let temp = options[i].cloneNode(true)
+                    let temp2 = options[i-1].cloneNode(true)
+                    options[i] = temp2
+                    options[i-1] = temp
+                    options[i-1].setAttribute('selected','selected')
+                    options[i].removeAttribute('selected')
+                    selected.splice($.inArray(selected[j], selected),1)
+                    break
+                }
+            }
+        }
+        console.log(options)
+        console.log(selected)
+        $("#imagesSelected").empty()
+        $("#imagesSelected").append(options)
+    })
+
+    $("#btnMoveDown").click(function (){
+        let options = $("#imagesSelected option")
+        let selected = $("#imagesSelected option:selected")
+        let selectedO = $("#imagesSelected option:selected")
+        for(let i=options.length-2; i>=0; i--){
+            for(let j=selected.length-1; j>=0; j--){
+                if(options[i] === selected[j]){
+                    let temp = options[i].cloneNode(true)
+                    let temp2 = options[i+1].cloneNode(true)
+                    options[i] = temp2
+                    options[i+1] = temp
+                    options[i+1].setAttribute('selected','selected')
+                    options[i].removeAttribute('selected')
+                    selected.splice($.inArray(selected[j], selected),1)
+                    break
+                }
+            }
+        }
+        for(let i=0;i<options.length;i++){
+            for(let j=0; j<selectedO.length; j++){
+                if(options[i]===selected[j]){
+                    options.prop("selected", "selected")
+                    break
+                }
+            }
+        }
+        console.log(options)
+        console.log(selected)
+        $("#imagesSelected").empty()
+        $("#imagesSelected").append(options)
+    })
 })
