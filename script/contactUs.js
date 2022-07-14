800ecb9133aa0ce9e0dc578f440d8b81b0bbf884
Petar Partaloski <ppartaloski@yahoo.com>
2022-07-14 09:53:41 +0200

    Improved icons

--- script/contactUs.js
+++ script/contactUs.js
@@ -1,9 +1,16 @@
 $(document).ready(function (){
     $("#carouselExampleSlidesOnly").hide()
 
+    $("#carouselExampleSlidesOnly2").hide()
+
     $("#expandContentBtn").click(function (){
         $("#carouselExampleSlidesOnly").slideDown();
         $("#expandContentBtn").toggleClass("d-none")
+    })
+
+    $("#expandContentBtn1").click(function (){
+        $("#carouselExampleSlidesOnly2").slideDown();
+        $("#expandContentBtn1").toggleClass("d-none")
     })
 
 
